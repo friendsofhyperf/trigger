4f368aec4bcd892ecbdc931d17b4c898a0bdd53d
hdj <hdj@addcn.com>
2021-01-25 08:52:31 +0800

    optimize

---
+++ src/Exception/ConfigNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of hyperf-trigger.
+ *
+ * @link     https://github.com/friendsofhyperf/trigger
+ * @document https://github.com/friendsofhyperf/trigger/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\Trigger\Exception;
+
+use Exception;
+
+class ConfigNotFoundException extends Exception
+{
+}
--- src/ReplicationFactory.php
+++ src/ReplicationFactory.php
@@ -10,7 +10,10 @@ declare(strict_types=1);
  */
 namespace FriendsOfHyperf\Trigger;
 
+use FriendsOfHyperf\Trigger\Exception\ConfigNotFoundException;
 use Hyperf\Contract\ConfigInterface;
+use MySQLReplication\Config\ConfigBuilder;
+use MySQLReplication\MySQLReplicationFactory;
 use Psr\Container\ContainerInterface;
 use RuntimeException;
 
@@ -22,7 +25,7 @@ class ReplicationFactory
     protected $config;
 
     /**
-     * @var Replication[]
+     * @var MySQLReplicationFactory[]
      */
     protected $replications = [];
 
@@ -38,8 +41,8 @@ class ReplicationFactory
     }
 
     /**
-     * @throws RuntimeException
-     * @return Replication
+     * @throws ConfigNotFoundException
+     * @return MySQLReplicationFactory
      */
     public function get(string $replication = 'default')
     {
@@ -47,7 +50,7 @@ class ReplicationFactory
             $key = 'trigger.' . $replication;
 
             if (! $this->config->has($key)) {
-                throw new RuntimeException('config ' . $key . ' is undefined.');
+                throw new ConfigNotFoundException('config ' . $key . ' is not found.');
             }
 
             $config = $this->config->get($key);
@@ -57,9 +60,43 @@ class ReplicationFactory
                 $config['binlog_position'] = $binLogCurrent->getBinLogPosition();
             }
 
-            $this->replications[$replication] = make(Replication::class, ['config' => $config]);
+            $this->replications[$replication] = $this->make($config);
         }
 
         return $this->replications[$replication];
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    public function make(array $config = []): MySQLReplicationFactory
+    {
+        $configBuilder = new ConfigBuilder();
+        $configBuilder->withUser($config['user'] ?? 'root')
+            ->withHost($config['host'] ?? '127.0.0.1')
+            ->withPassword($config['password'] ?? 'root')
+            ->withPort((int) $config['port'] ?? 3306)
+            ->withSlaveId($this->generateSlaveId())
+            ->withHeartbeatPeriod((float) $config['heartbeat_period'] ?? 3)
+            ->withDatabasesOnly((array) $config['databases_only'] ?? [])
+            ->withtablesOnly((array) $config['tables_only'] ?? []);
+
+        if (isset($config['binlog_filename'])) {
+            $configBuilder->withBinLogFileName($config['binlog_filename']);
+        }
+
+        if (isset($config['binlog_position'])) {
+            $configBuilder->withBinLogPosition((int) $config['binlog_position']);
+        }
+
+        return new MySQLReplicationFactory($configBuilder->build());
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    protected function generateSlaveId(): int
+    {
+        return (int) ip2long(Util::getInternalIp());
     }
 }

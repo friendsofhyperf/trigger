f570ff07e61c8e431e016516f6588928a7383d03
huangdijia <huangdijia@gmail.com>
2021-06-24 14:42:31 +0800

    Optimized

--- src/Subscriber/TriggerSubscriber.php
+++ src/Subscriber/TriggerSubscriber.php
@@ -13,6 +13,7 @@ namespace FriendsOfHyperf\Trigger\Subscriber;
 use FriendsOfHyperf\Trigger\ChannelManager;
 use FriendsOfHyperf\Trigger\TriggerManager;
 use Hyperf\Contract\ConfigInterface;
+use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Utils\Coroutine;
 use Hyperf\Utils\Coroutine\Concurrent;
 use MySQLReplication\Definitions\ConstEventsNames;
@@ -48,12 +49,24 @@ class TriggerSubscriber extends AbstractSubscriber
      */
     protected $config;
 
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var StdoutLoggerInterface
+     */
+    protected $logger;
+
     public function __construct(ContainerInterface $container, string $replication = 'default')
     {
         $this->replication = $replication;
+        $this->container = $container;
         $this->config = $container->get(ConfigInterface::class);
         $this->chan = $container->get(ChannelManager::class)->get($replication);
         $this->triggerManager = $container->get(TriggerManager::class);
+        $this->logger = $container->get(StdoutLoggerInterface::class);
         $this->concurrent = new Concurrent(
             (int) $this->config->get(sprintf('trigger.%s.trigger.current', $replication), 1000)
         );
@@ -96,15 +109,24 @@ class TriggerSubscriber extends AbstractSubscriber
         foreach ($this->triggerManager->get($key) as $callable) {
             foreach ($event->getValues() as $value) {
                 $this->concurrent->create(function () use ($callable, $value, $eventType) {
+                    [$class, $method] = $callable;
+
+                    if (! $this->container->has($class)) {
+                        $this->logger->warning(sprintf('%s cannot get instance from container.', $class));
+                        return;
+                    }
+
+                    $instance = $this->container->get($class);
+
                     switch ($eventType) {
                         case ConstEventsNames::WRITE:
-                            call($callable, [$value]);
+                            call([$instance, $method], [$value]);
                             break;
                         case ConstEventsNames::UPDATE:
-                            call($callable, [$value['before'], $value['after']]);
+                            call([$instance, $method], [$value['before'], $value['after']]);
                             break;
                         case ConstEventsNames::DELETE:
-                            call($callable, [$value]);
+                            call([$instance, $method], [$value]);
                             break;
                     }
                 });

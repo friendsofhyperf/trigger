af12419e5aff0abc2bcc40f59fc7ed59631a561d
huangdijia <huangdijia@gmail.com>
2021-07-14 13:05:49 +0800

    Optimized

--- src/EventDispatcher.php
+++ src/EventDispatcher.php
@@ -35,8 +35,9 @@ class EventDispatcher extends \Symfony\Component\EventDispatcher\EventDispatcher
 
     public function dispatch(object $event, ?string $eventName = null): object
     {
+        co(fn () => $this->process->getHealthMonitor()->setBinLogCurrent($event->getEventInfo()->getBinLogCurrent()));
+
         $this->concurrent->create(function () use ($event, $eventName) {
-            co(fn () => $this->process->getHealthMonitor()->setBinLogCurrent($event->getEventInfo()->getBinLogCurrent()));
             parent::dispatch($event, $eventName);
         });
 
